5a06554621e10c196d7f897c45e8de1345e5dcd6
Nguyen Van Anh <vananh080104@gmail.com>
2025-04-20 15:08:36 +0700

    Hoan thanh loginbladephp

--- resources/views/front/login.blade.php
+++ resources/views/front/login.blade.php
@@ -198,6 +198,60 @@
         </div>
     <!-- Header End -->
 
+     <!-- Breadcrumb Section Begin -->
+     <div class="breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text">
+                        <a href="login.blade.php"><i class="fa fa-home"></i> Home</a>
+                        <span>Login</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section End -->
+
+    <!-- Login Section Begin-->
+<section class="login-register-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+                <div class="login-form">
+                    <h2>Login</h2>
+                    <form action="#" method="POST">
+                        <div class="group-input">
+                            <label for="username">Username or email address  *</label>
+                            <input type="text" id="username" name="username" placeholder="Enter your username or email">
+                        </div>
+                        <div class="group-input">
+                            <label for="password">Password *</label>
+                            <input type="password" id="password" name="password" placeholder="Enter your password">
+                        </div>
+                        <div class="group-input gi-check">
+                            <div class="gi-more">
+                                <label for="save-pass">
+                                    Save Password
+                                    <input type="checkbox" id="save-pass">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <a href="#" class="forget-pass">Forget your Password?</a>
+                            </div>
+                        </div>
+                        <button type="submit" class="site-btn login-btn">Sign In</button>
+                    </form>
+                    <div class="switch-login">
+                        <a href="register.html" class="or-login">Or Create An Account</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Login Section End-->
+
+
     <!-- Partner Logo Section Begin -->
     <div class="partner-logo">
         <div class="container">
