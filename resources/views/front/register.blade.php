b9914c7e3708d262d72c88434fb3b17e2cb549c4
Nguyen Van Anh <vananh080104@gmail.com>
2025-04-20 15:48:50 +0700

    hoan thanh register

--- resources/views/front/register.blade.php
+++ resources/views/front/register.blade.php
@@ -198,6 +198,54 @@
         </div>
     <!-- Header End -->
 
+      <!-- Breadcrumb Section Begin -->
+      <div class="breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text">
+                        <a href="register.blade.php"><i class="fa fa-home"></i> Home</a>
+                        <span>Register</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section End -->
+
+    <!-- Register Section Begin -->
+    <!-- Register Section Begin -->
+<section class="login-register-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+                <div class="register-form">
+                    <h2>Register</h2>
+                    <form action="#" method="POST">
+                        <div class="group-input">
+                            <label for="username">Username or email address *</label>
+                            <input type="text" id="username" name="username" placeholder="Enter your username or email">
+                        </div>
+                        <div class="group-input">
+                            <label for="password">Password *</label>
+                            <input type="password" id="password" name="password" placeholder="Enter your password">
+                        </div>
+                        <div class="group-input">
+                            <label for="confirm-password">Confirm Password *</label>
+                            <input type="password" id="confirm-password" name="confirm_password" placeholder="Confirm your password">
+                        </div>
+                        <button type="submit" class="site-btn login-btn" style="background-color: #d59f2a;">Register</button>
+                    </form>
+                    <div class="switch-login">
+                        <a href="login.html" class="or-login">Or Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Register Section End -->
+
     <!-- Partner Logo Section Begin -->
     <div class="partner-logo">
         <div class="container">
